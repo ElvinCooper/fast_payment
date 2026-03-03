3ac9b2b71d594cab2b5da5b4b33fa5159876ce44
Elvin cooper <ing.elvin01cooper@gmail.com>
2026-03-03 11:43:53 -0400

    add: __init__ para importacion seguro, ruta de busqueda config vercel simplificada

--- api/index.py
+++ api/index.py
@@ -1,1 +1,8 @@
+import os
+import sys
+from fastapi import FastAPI
+
+# Añadir el directorio raíz al path para que Python encuentre la carpeta 'app'
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app.main import app
